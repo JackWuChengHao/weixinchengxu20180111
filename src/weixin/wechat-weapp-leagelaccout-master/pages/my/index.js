67109db35b34acdbbfd738f78af459e96fdcbafd
xiaowu <2457893105@qq.com>
2018-01-24 01:58:10 +0800

    更新微信小程序端

--- src/weixin/wechat-weapp-leagelaccout-master/pages/my/index.js
+++ src/weixin/wechat-weapp-leagelaccout-master/pages/my/index.js
@@ -1,85 +1,37 @@
 //获取应用实例
 var app = getApp()
 Page({
-    data: {
-        indicatorDots: true,
-        vertical: false,
-        autoplay: true,
-        interval: 3000,
-        duration: 1000,
-        loadingHidden: false  // loading
+    data:{
+      userInfo: {},
+      userListInfo: [
+        {
+          icon: '../../images/questionlist.png',
+          text: '我的问题',
+          isunread: true,
+          unreadNum: 1
+        },
+        {
+          icon: '../../images/systemmsg.png',
+          text: '系统消息',
+          isunread: false,
+          unreadNum: 1
+        },
+        {
+          icon: '../../images/customer service.png',
+          text: '联系客服'
+        }]
     },
 
-    //事件处理函数
-    swiperchange: function(e) {
-        //console.log(e.detail.current)
-    },
 
-    onLoad: function() {
-        console.log('onLoad')
-        var that = this
-            //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo
-            })
+
+      onLoad: function () {
+      var that = this
+      //调用应用实例的方法获取全局数据
+      app.getUserInfo(function (userInfo) {
+        //更新数据
+        that.setData({
+          userInfo: userInfo
         })
-
-        //sliderList
-        wx.request({
-            url: 'https://huanqiuxiaozhen.com/wemall/slider/list',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                that.setData({
-                    images: res.data
-                })
-            }
-        })
-
-        //venuesList
-        wx.request({
-            url: 'https://huanqiuxiaozhen.com/wemall/venues/venuesList',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                that.setData({
-                    venuesItems: res.data.data
-                })
-                setTimeout(function () {
-                    that.setData({
-                        loadingHidden: true
-                    })
-                }, 1500)
-            }
-        })
-
-        //choiceList
-        wx.request({
-            url: 'https://huanqiuxiaozhen.com/wemall/goods/choiceList',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                that.setData({
-                    choiceItems: res.data.data.dataList
-                })
-                setTimeout(function () {
-                    that.setData({
-                        loadingHidden: true
-                    })
-                }, 1500)
-            }
-        })
-
+      })
     }
 })
